2c2ef58b0919a4ce1822237990b2e2e6c18476e9
schizoneko <viettung11122004@gmail.com>
2025-04-11 12:09:35 +0700

    ADD 20242 semester's project's structure

---
+++ Project_20242/inc/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Standard libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <ctype.h>
+
+// Params
+
+
+#endif // config.h
